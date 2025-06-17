10a35cb695512e0e61ba2f825f2ab6d9fa811c56
deni zakya <denizakya@gmail.com>
2025-06-17 21:48:42 +0200

    undo send_calibration function modification

--- src/common/rawdata.c
+++ src/common/rawdata.c
@@ -281,7 +281,6 @@ static uint8_t * copy_lsb_first(uint8_t *dst, float f)
 	*dst++ = data.n >> 24;
 	return dst;
 }
-/*
 
 int send_calibration(void)
 {
@@ -332,61 +331,6 @@ int send_calibration(void)
 	}
 	*p++ = crc;   // 2 byte crc check
 	*p++ = crc >> 8;
-	// return write_serial_data(buf, 68);
 	return write_ipc_file_data(buf, 68);
-}
-*/
-
-// Hàm thay đổi: trả về độ dài và nhận buf làm đối số
-int send_calibration(uint8_t *buf_out)
-{
-    uint8_t *p = buf_out;
-    uint16_t crc;
-    int i;
-
-    *p++ = 117; // 2 byte signature
-    *p++ = 84;
-
-    for (i=0; i < 3; i++) {
-        p = copy_lsb_first(p, 0.0f); // accelerometer offsets
-        cal_data_sent[0+i] = 0.0f;
-    }
-    for (i=0; i < 3; i++) {
-        p = copy_lsb_first(p, 0.0f); // gyroscope offsets
-        cal_data_sent[3+i] = 0.0f;
-    }
-    for (i=0; i < 3; i++) {
-        p = copy_lsb_first(p, magcal.V[i]); // 12 bytes offset/hardiron
-        cal_data_sent[6+i] = magcal.V[i];
-    }
-    p = copy_lsb_first(p, magcal.B); // field strength
-    p = copy_lsb_first(p, magcal.invW[0][0]); //10
-    p = copy_lsb_first(p, magcal.invW[1][1]); //11
-    p = copy_lsb_first(p, magcal.invW[2][2]); //12
-    p = copy_lsb_first(p, magcal.invW[0][1]); //13
-    p = copy_lsb_first(p, magcal.invW[0][2]); //14
-    p = copy_lsb_first(p, magcal.invW[1][2]); //15
-
-    cal_data_sent[9]  = magcal.B;
-    cal_data_sent[10] = magcal.invW[0][0];
-    cal_data_sent[11] = magcal.invW[0][1];
-    cal_data_sent[12] = magcal.invW[0][2];
-    cal_data_sent[13] = magcal.invW[1][0];
-    cal_data_sent[14] = magcal.invW[1][1];
-    cal_data_sent[15] = magcal.invW[1][2];
-    cal_data_sent[16] = magcal.invW[2][0];
-    cal_data_sent[17] = magcal.invW[2][1];
-    cal_data_sent[18] = magcal.invW[2][2];
-
-    cal_confirm_needed = 3;
-
-    crc = 0xFFFF;
-    for (i = 0; i < 66; i++) {
-        crc = crc16(crc, buf_out[i]);
-    }
-    *p++ = crc;
-    *p++ = crc >> 8;
-    // return write_serial_data(buf, 68);
-    return 68; // Trả về độ dài buffer đã tạo
 }
 
