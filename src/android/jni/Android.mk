de24d30aaaf73b4cda2761ee1fb20466b7f21f7a
deni zakya <denizakya@gmail.com>
2025-06-21 09:16:28 +0200

    update to build for specific android

--- src/android/jni/Android.mk
+++ src/android/jni/Android.mk
@@ -3,17 +3,20 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := motioncalibration
-LOCAL_SRC_FILES := motioncalibration_jni.c \
-                   ../../common/quality.c \
-                   ../../common/motioncalibration.c \
-				   ../../common/magcal.c \
-				   ../../common/mahony.c \
-				   ../../common/matrix.c \
-				   ../../common/rawdata.c \
-				   ../../common/serialdata.c \
-				   ../../common/visualize.c 
+
+LOCAL_SRC_FILES := \
+    motioncalibration_jni.c \
+    ../../common/motioncalibration.c \
+    ../../common/mahony.c \
+    ../../common/matrix.c \
+    ../../common/magcal.c \
+    ../../common/quality.c \
+    ../../common/rawdata.c \
+    ../../common/serialdata.c \
+    ../../common/visualize.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../common
+
 LOCAL_LDLIBS := -llog -lm
 
 include $(BUILD_SHARED_LIBRARY)
